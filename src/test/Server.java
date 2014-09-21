dd36344284962d4458065f575e7aa89bce42c7ad
Chao Zhang <zhangchao6865@gmail.com>
2014-09-20 20:52:47 -0400

    Now able to lookup

--- src/test/Server.java
+++ src/test/Server.java
@@ -1,13 +1,15 @@
 package test;
 
 import rmi.core.Unicast;
+import rmi.registry.LocateRegistry;
+import rmi.registry.Registry;
 
 public class Server {
     public static void main(String args[]) {
         Hello h = new HelloImp();
-
-        Unicast.export(h, 12345);
-
+        Hello stub = (Hello)Unicast.export(h, 12345);
+        Registry registry = LocateRegistry.getRegistry("localhost", 3044);
+        registry.rebind("Hello", stub);
         while (true);
     }
 }
