ae61a84418b04695ed206e6224bbd1179c689e94
Chao Zhang <zhangchao6865@gmail.com>
2014-09-21 13:36:30 -0400

    Move back

---
+++ src/rmi/core/RemoteException.java
@@ -0,0 +1,24 @@
+package rmi.core;
+
+/**
+ * Remote Exception
+ * 
+ * @author Chao
+ *
+ */
+public class RemoteException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5775213995740956172L;
+	
+	public RemoteException() {
+		
+	}
+	
+	public RemoteException(String message) {
+		super(message);
+	}
+
+}
