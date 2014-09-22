31a0369f7b4a623ef65d7f8416857f7f1aeb3585
cxhiano <cxhiano@gmail.com>
2014-09-21 22:35:16 -0400

    fix comment

--- src/rmi/core/UnicastRemoteObject.java
+++ src/rmi/core/UnicastRemoteObject.java
@@ -4,12 +4,22 @@ import java.lang.reflect.Proxy;
 import java.io.IOException;
 
 /**
- * UnicastRemoteObject
+ * Used for exporting a remote object and obtaining its client-side stub. On
+ * client-side, all * method invocations of object obj are intercepted. Instead
+ * of invoking the 'real' method, the proxy forwards the data associated with
+ * the invocation to the host where the 'real' object is served.
  *
- * @author Chao
+ * @author Chao Xin, Chao Zhang
  *
  */
 public class UnicastRemoteObject {
+   /**
+     * Generate the stub for object obj by using Proxy.
+     *
+     * @param obj
+     * @param ref
+     * @return Stub for obj
+     */
     private static Remote generateStub(Remote obj, RemoteObjectRef ref) {
         RemoteInvocationHandler handler = new RemoteInvocationHandler(ref);
         Remote proxy = (Remote) Proxy.newProxyInstance(
